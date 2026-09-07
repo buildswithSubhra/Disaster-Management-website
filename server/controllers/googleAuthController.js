32f06918be0db4af143510ab97f110a8da5182f0
Subhradip Das <subhradipd150@gmail.com>
2026-09-07 13:20:35 +0530

    fix: use single FIREBASE_SERVICE_ACCOUNT env var for Render compatibility

--- server/controllers/googleAuthController.js
+++ server/controllers/googleAuthController.js
@@ -3,13 +3,17 @@ const { generateToken } = require('../utils/helpers');
 const { initializeApp, cert, getApps } = require('firebase-admin');
 
 if (!getApps().length) {
-  initializeApp({
-    credential: cert({
+  let serviceAccount;
+  if (process.env.FIREBASE_SERVICE_ACCOUNT) {
+    serviceAccount = JSON.parse(process.env.FIREBASE_SERVICE_ACCOUNT);
+  } else {
+    serviceAccount = {
       projectId: process.env.FIREBASE_PROJECT_ID,
       clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
       privateKey: process.env.FIREBASE_PRIVATE_KEY?.replace(/\\n/g, '\n'),
-    }),
-  });
+    };
+  }
+  initializeApp({ credential: cert(serviceAccount) });
 }
 
 const { getAuth } = require('firebase-admin/auth');
